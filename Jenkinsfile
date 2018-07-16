cafff0a828d61cec491ed22cabf557d4820084d9
Andrea Alvarez <dry.lez@gmail.com>
2018-07-16 11:07:35 -0600

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,7 +5,7 @@ pipeline{
             steps{
                 echo "building..."
                 sh '''eval $(aws ecr get-login --no-include-email --region us-west-2 | sed \'s|https://||\')
-                docker build -t jenkins-app .'''
+                docker build -t jenkins-app ./node'''
             }
         }
         stage('test'){
--- nodeweb/routes/routes.js
+++ nodeweb/routes/routes.js
@@ -1,8 +1,12 @@
 var redis = require('redis');
 var client = redis.createClient('6379', process.env.REDIS_URL);
 
+client.on("error", function(err) {
+    console.error("Error connecting to redis", err);
+});
+
 exports.index = function(req, res){
-    res.render('index', { title: 'App-Andre' });
+        res.render('index', { title: 'App-Andre' });
 };
 
 exports.saveUser = function(req, res) {
@@ -10,29 +14,33 @@ exports.saveUser = function(req, res) {
     newUser.user = req.body['user_txt'];
     newUser.username = req.body['name_txt'];
     newUser.pass = req.body['pass_txt'];
-   
-    client.hset('Users', "id."+newUser.user, "User: "+newUser.user + " | Name: "+ newUser.username + " | Password: " + newUser.pass);
-    res.redirect("/");
+    if ( client != null) {
+        client.hset('Users', "id."+newUser.user, "User: "+newUser.user + " | Name: "+ newUser.username + " | Password: " + newUser.pass);
+        res.redirect("/");
+    }
 };
 
 exports.getUsers = function (req, res){
     var users = [];
-    client.hgetall("Users", function(err, objs) {
-        if(objs){
-            for(var u in objs) {
+    if ( client!=null) {
+        client.hgetall("Users", function(err, objs) {
+            if(objs){
+                for(var u in objs) {
+                    var newUser = {
+                        text: objs[u],
+                        id:u
+                    };
+                    users.push(newUser);
+                }
+            }else{
                 var newUser = {
-                    text: objs[u],
-                    id:u
+                    text: "No hay usuarios",
+                    id:"0"
                 };
                 users.push(newUser);
             }
-        }else{
-            var newUser = {
-                text: "No hay usuarios",
-                id:"0"
-            };
-            users.push(newUser);
-        }
+            
+        });
         res.render('index', {title: 'App-Andre', users: users});
-    });
+    }
 }
